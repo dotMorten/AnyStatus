54e30cf1a97000e25b2f52fa6237b2f597941a8c
Morten Nielsen <mn@iter.dk>
2021-04-10 23:21:02 -0700

    Added custom nuget version checker

---
+++ src/Plugins/AnyStatus.Plugins.dotMorten/NuGetPackageQuery.cs
@@ -0,0 +1,46 @@
+﻿using AnyStatus.API.Widgets;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AnyStatus.Plugins.dotMorten
+{
+    public class NuGetPackageQuery : AsyncStatusCheck<NuGetPackageVersionWidget>
+    {
+        private static object lockObj = new object();
+        private static System.DateTime clientCreationTime = System.DateTime.Now;
+        private static HttpClient client = new HttpClient(new SocketsHttpHandler() { AutomaticDecompression = System.Net.DecompressionMethods.GZip });
+
+        protected override async Task Handle(StatusRequest<NuGetPackageVersionWidget> request, CancellationToken cancellationToken)
+        {
+            lock (lockObj)
+            {
+                if ((System.DateTime.Now - clientCreationTime).TotalMinutes > 10)
+                {
+                    // Recycle client every 10 minutes
+                    clientCreationTime = System.DateTime.Now;
+                    client = new HttpClient(new SocketsHttpHandler() { AutomaticDecompression = System.Net.DecompressionMethods.GZip });
+                }
+            }
+            request.Context.Status = Status.Running;
+            var uri = "https://api.nuget.org/v3/registration5-gz-semver2/" + request.Context.PackageId.ToLower() + "/index.json";
+            var result = await client.GetStringAsync(uri);
+            Root metadata = JsonConvert.DeserializeObject<Root>(result);
+            var version = metadata?.items?.LastOrDefault()?.upper;
+            request.Context.Text = version;
+            request.Context.Status = Status.OK;
+        }
+        private class Root
+        {
+            public List<ItemsItem> items { get; set; }
+        }
+        private class ItemsItem
+        {
+            public List<ItemsItem> items { get; set; }
+            public string upper { get; set; }
+        }
+    }
+}
---
+++ src/Plugins/AnyStatus.Plugins.dotMorten/NuGetPackageVersionWidget.cs
@@ -0,0 +1,19 @@
+﻿using AnyStatus.API.Attributes;
+using AnyStatus.API.Widgets;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace AnyStatus.Plugins.dotMorten
+{
+    [Category("Morten")]
+    [DisplayName("NuGet Package Version")]
+    [Description("NuGet package version number")]
+    public class NuGetPackageVersionWidget : TextLabelWidget, IPollable, IStandardWidget
+    {
+        [Order(10)]
+        [Required]
+        [DisplayName("NuGet Package")]
+        [Description("The NuGet package id. For example: AnyStatus.API")]
+        public string PackageId { get; set; }
+    }
+}
